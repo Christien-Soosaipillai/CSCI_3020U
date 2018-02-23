1c077a753c141877bdd0e96c16f043bb5546928c
miral-panchal <miral.panchal@uoit.net>
2018-02-23 18:39:21 -0500

    Finished Part 1 Except for cd

--- Labs/Lab_2/myshell_example/myshell.c
+++ Labs/Lab_2/myshell_example/myshell.c
@@ -3,12 +3,13 @@
  *
  * Copyright (C) 2017, <GROUP MEMBERS>
  * All rights reserved.
- * 
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
 #include "utility.h"
@@ -24,20 +25,20 @@
 
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
     // Input buffer and and commands
     char buffer[BUFFER_LEN] = { 0 };
     char command[BUFFER_LEN] = { 0 };
     char arg[BUFFER_LEN] = { 0 };
-    
+
     // input argument variables
-    
-        
+
+
     // Parse the commands provided using argc and argv
 
     // Perform an infinite loop getting command input from users
-    
+
     // for ( int i = 0; i < argc; i++) {
     //     strcpy (buffer,argv[i]);
     // }
@@ -45,7 +46,7 @@ int main(int argc, char *argv[])
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
     {
         // Perform string tokenization to get the command and argument
-        
+
 
 
         if( strstr(buffer, " ")!=NULL) {
@@ -70,35 +71,59 @@ int main(int argc, char *argv[])
         // cd command -- change the current directory
         if (strcmp(command, "cd") == 0)
         {
-            if(arg == NULL){
-                printf("ARGUMENT MISSING");
+          if(arg == NULL){
+            char cwd[BUFFER_LEN] = { 0 };
+            printf("%s\n", getcwd(cwd,sizeof(cwd)));
+          }
+          else if (arg != NULL){
+            struct stat sb;
+            if(stat(arg, &sb) == 0 && S_ISDIR(sb.st_mode)){
+              chdir(arg);
+              setenv("PWD",arg,1);
             }
-            else if (arg != NULL){
-                printf("%s", arg);
+            else{
+              printf("%s\n", "ERROR: Directory doesn't exist.");
             }
-            printf("\n");
-            // chdir(arg);
+          }
         }
          else if (strcmp(command, "clr") == 0) {
-         
+           system("clear");
         }
 
         else if (strcmp(command, "dir") == 0) {
-          
-        }
-        
+          struct dirent *de;  // Pointer for directory entry
+
+          // opendir() returns a pointer of DIR type.
+          DIR *dr = opendir(".");
+
+          if (dr == NULL)  // opendir returns NULL if couldn't open directory
+          {
+            printf("Could not open current directory" );
+            return 0;
+          }
+
+          while ((de = readdir(dr)) != NULL)
+            printf("%s\n", de->d_name);
+
+        closedir(dr);
+      }
+
         else if (strcmp(command, "environ") == 0) {
-            
+
+          for (int i = 0; envp[i] != NULL; i++) {
+            printf("\n%s", envp[i]);
+          }
+          printf("\n");
         }
-        
+
         else if (strcmp(command, "echo") == 0) {
 
             printf("%s\n",arg);
-                
+
         }
 
         else if (strcmp(command, "pause") == 0) {
-            
+
             printf("Press a key to continue");
             char key = NULL;
             while(key== NULL){
